92d5fae7610d3a819c3b03bff9f241bc887d9028
Exuar Ivan Gatica <exuar1gatica@Exuars-MacBook-Pro.local>
2014-06-26 13:05:47 -0700

    API Calls

--- features/api_testing.feature
+++ features/api_testing.feature
@@ -3,3 +3,13 @@ Feature: looking up a movie whit rest service
   Scenario:  using rotten tomatoes to look up movie data
     Given a user checks rotten tommats for a movie
     Then Movie data is return
+
+  Scenario Outline: validating toy story 3 data
+    Given user goes to rotten tomatoes for toy story
+    Then user retrieves and validates "<data>" of toy story with "<correct_info>"
+  Examples:
+    | data | correct_info |
+    | title | Toy Story 3 |
+    | year  |  2010       |
+    |mpaa_rating|  G      |
+    | runtime|    103     |
--- features/step_definitions/my_steps.rb
+++ features/step_definitions/my_steps.rb
@@ -2,7 +2,19 @@ Given(/^a user checks rotten tommats for a movie$/) do
   @mquery = JSON.parse(open("http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=up33xn2kyj88xw2tgfek3ca9&q=Toy+Story+3&page_limit=1").read)
 end
 
-
 Then(/^Movie data is return$/) do
- puts @mquery['movies'][0]
+ puts @mquery['movies'][0]['title']
+end
+
+
+Given(/^user goes to rotten tomatoes for toy story$/) do
+  @mquery = JSON.parse(open("http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=up33xn2kyj88xw2tgfek3ca9&q=Toy+Story+3&page_limit=1").read)
+end
+
+Then(/^user retrieves and validates "([^"]*)" of toy story with "([^"]*)"$/) do |data, correct_info|
+  data_retrived = @mquery['movies'][0]["#{data}"]
+ puts x= "#{data_retrived}"
+  puts y= "#{correct_info}"
+  assert x == y
+
 end
